20608652ebb6a459e0dbab83461925e03b918413
David <neurofibromatose@live.nl>
2015-03-11 14:43:41 +0100

    Dutch translation

---
+++ language/nl/ucp.php
@@ -0,0 +1,24 @@
+<?php
+/**
+*
+* @package phpBB Extension - Bubble Mouse - English
+* @copyright (c) 2015
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* Dutch translation by Dutch Translators (https://github.com/dutch-translators)
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'BUBBLEMOUSE'			=> 'Gebruik Bubbel muis',
+	'BUBBLEMOUSE_EXPLAIN'	=> 'Door "ja" te selecteren krijgt je cursor een leuk bubbelend effect.',
+));
