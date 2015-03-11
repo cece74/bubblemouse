e017fa7a742cfcbaf01803a0eb035c7676364bf3
cece74 <forumacquariofilia@libero.it>
2015-03-11 11:29:18 +0100

    Create cece74_maxwidthswitch_migrations.php

---
+++ migrations/cece74_maxwidthswitch_migrations.php
@@ -0,0 +1,35 @@
+<?php
+/**
+*
+* @package phpBB Extension - Bubble Mouse
+* @copyright (c) 2015
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace cece74\bubblemouse\migrations;
+
+class crizzo_maxwidthswitch_migrations extends \phpbb\db\migration\migration
+{
+	public function update_schema()
+	{
+		return array(
+			'add_columns'		=> array(
+				$this->table_prefix . 'users'	=> array(
+					'user_maxwidth'		=> array('BOOL', 0, 'after' => 'user_options'),
+				),
+			),
+		);
+	}
+
+	public function revert_schema()
+	{
+		return array(
+			'drop_columns'		=> array(
+				$this->table_prefix . 'users'	=> array(
+					'user_maxwidth',
+				),
+			),
+		);
+	}
+}
